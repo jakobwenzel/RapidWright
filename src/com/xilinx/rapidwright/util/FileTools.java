9ef5b4a7e79ce1a4804c3947a8e0a7fa1196dfdf
Jakob Wenzel <jakobw@xilinx.com>
2021-06-08 14:51:18 +0200

    Don't exit the Program in case of File IO Exceptions
    Signed-off-by: Jakob Wenzel <jakobw@xilinx.com>

--- src/com/xilinx/rapidwright/util/FileTools.java
+++ src/com/xilinx/rapidwright/util/FileTools.java
@@ -192,11 +192,8 @@ public class FileTools {
 				in = new BufferedReader(new FileReader(fileName));	
 			}			
 		}
-		catch(FileNotFoundException e){
-			MessageGenerator.briefErrorAndExit("ERROR: Could not find file: " + fileName);
-		}
 		catch(IOException e){
-			e.printStackTrace();
+			throw new UncheckedIOException(e);
 		}
 
 		return in;
@@ -429,8 +426,7 @@ public class FileTools {
 			bw.close();
 		}
 		catch(IOException e){
-			MessageGenerator.briefErrorAndExit("Error writing file: " +
-				fileName + File.separator + e.getMessage());
+			throw new UncheckedIOException(e);
 		}
 	}
 	
@@ -447,8 +443,7 @@ public class FileTools {
 			bw.close();
 		}
 		catch(IOException e){
-			MessageGenerator.briefErrorAndExit("Error writing file: " +
-				fileName + File.separator + e.getMessage());
+			throw new UncheckedIOException(e);
 		}
 	}
 	
@@ -468,11 +463,8 @@ public class FileTools {
 				lines.add(line);
 			}
 		}
-		catch(FileNotFoundException e){
-			MessageGenerator.briefErrorAndExit("ERROR: Could not find file: " + fileName);
-		} 
 		catch(IOException e){
-			MessageGenerator.briefErrorAndExit("ERROR: Could not read from file: " + fileName);
+			throw new UncheckedIOException(e);
 		}
 		
 		return lines;
@@ -1195,11 +1187,10 @@ public class FileTools {
 			p.waitFor();
 		    return lines;
 		} catch (IOException e) {
-			e.printStackTrace();
+			throw new UncheckedIOException(e);
 		} catch (InterruptedException e) {
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
-		return null;
 	}
 
 	/**
